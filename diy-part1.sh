7ff09ab268330ceea63051ebacfcdaec1ee8b7db
chihwell <72851760+chihwell@users.noreply.github.com>
2021-08-21 23:59:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git jd-dailybonus https://github.com/jerrykuku/luci-app-jd-dailybonus' >>feeds.conf.default
-echo 'src-git ssr-plus https://github.com/maxlicheng/luci-app-ssr-plus' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
